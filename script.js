e6d47ba02f403ae9c582169a23320753c40a3cf9
cosmiceiling <mylastone7612@gmail.com>
2025-11-04 23:16:30 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("loveButton").addEventListener("click", function() {
+  const loveText = document.getElementById("loveText");
+  loveText.classList.remove("hidden");
+  loveText.textContent = "I Love You Anggunkuuu :3 💙";
+});
