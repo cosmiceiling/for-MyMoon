efe959e9a2c1a83756145248a66334e9194292e4
cosmiceiling <mylastone7612@gmail.com>
2025-11-04 23:40:34 +0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("loveButton").addEventListener("click", function() {
-  const loveText = document.getElementById("loveText");
-  loveText.classList.remove("hidden");
-  loveText.textContent = "I Love You Anggunkuuu :3 💙";
-});
